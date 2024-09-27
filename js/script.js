f97fdc50193a8819427418a7ff570c5740d12492
speedyjohn <nachprog@gmail.com>
2024-09-27 22:19:18 +0500

    Only 2 week deadlines

--- js/script.js
+++ js/script.js
@@ -10,29 +10,32 @@ document.getElementById('title').innerText = `Deadlines for ${fname} ${lname}`
 // Completing deadlines
 const deadlinesContainer = document.getElementById("deadlines");
 for (const [courseName, deadlines] of Object.entries(data)) {
-    const courseContainer = document.createElement('div')
-    courseContainer.classList.add('course')
-    const courseHeader = document.createElement('h1');
-    courseHeader.classList.add('course-title')
-    courseHeader.textContent = courseName;
-    deadlinesContainer.appendChild(courseContainer)
-    courseContainer.appendChild(courseHeader);
+    console.log(deadlines)
+    if(Object.keys(deadlines).length > 0) {
+        const courseContainer = document.createElement('div')
+        courseContainer.classList.add('course')
+        const courseHeader = document.createElement('h1');
+        courseHeader.classList.add('course-title')
+        courseHeader.textContent = courseName;
+        deadlinesContainer.appendChild(courseContainer)
+        courseContainer.appendChild(courseHeader);
 
 
-    const sortedDeadlines = Object.entries(deadlines).sort((a, b) => a[1] - b[1]);
-    for (const [assignmentName, timestamp] of sortedDeadlines) {
-        const deadlineParagraph = document.createElement('p');
-        deadlineParagraph.classList.add("course-paragraph")
+        const sortedDeadlines = Object.entries(deadlines).sort((a, b) => a[1] - b[1]);
+        for (const [assignmentName, timestamp] of sortedDeadlines) {
+            const deadlineParagraph = document.createElement('p');
+            deadlineParagraph.classList.add("course-paragraph")
 
-        const deadlineDate = timestamp * 1000;
-        const formattedDate = formatTimestamp(deadlineDate);
-        const timeDifference = deadlineDate - Date.now();
-        const formattedTimeDifference = formatTimeDifference(timeDifference);
+            const deadlineDate = timestamp * 1000;
+            const formattedDate = formatTimestamp(deadlineDate);
+            const timeDifference = deadlineDate - Date.now();
+            const formattedTimeDifference = formatTimeDifference(timeDifference);
 
 
-        // Устанавливаем текст для параграфа
-        deadlineParagraph.innerHTML = `<span class="assignment-name">${assignmentName}:</span>  ${formattedDate}</br> <span class="assignment-timeleft">${formattedTimeDifference} before the deadline</span>`;
+            // Устанавливаем текст для параграфа
+            deadlineParagraph.innerHTML = `<span class="assignment-name">${assignmentName}:</span>  ${formattedDate}</br> <span class="assignment-timeleft">${formattedTimeDifference} before the deadline</span>`;
 
-        courseContainer.appendChild(deadlineParagraph);
+            courseContainer.appendChild(deadlineParagraph);
+        }
     }
 }
